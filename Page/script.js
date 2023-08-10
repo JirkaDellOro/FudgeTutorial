9f419eb402add42320fe89165e77da124c883827
Maurice Huchler <maurice.huchler@web.de>
2023-08-10 09:58:00 +0200

    added sidebar

--- Page/script.js
+++ Page/script.js
@@ -1,3 +1,18 @@
+//sidebar
+function sidebar_switch() {
+  let style = document.getElementById("SideBar_Content").style;
+  if (style.display == "none") {
+    style.display = "block";
+  } else {
+    style.display = "none";
+  }
+}
+
+function sidebar_close() {
+  document.getElementById("SideBar_Content").style.display = "none";
+}
+
+// image
 function openOverlay(imageSrc) {
   const overlay = document.querySelector(".overlay");
   const overlayImage = document.querySelector("#overlayImage");
@@ -12,12 +27,15 @@ function closeOverlay() {
   const body = document.querySelector("body");
 
   overlay.style.display = "none";
-  body.style.overflow ="visible";
+  body.style.overflow = "visible";
 }
 
 window.addEventListener("load", function () {
   let polys = this.document.getElementsByTagName("img");
   for (let i = 0; i < polys.length; i++) {
+    if (i == 0 || i == 1) {
+      continue;
+    }
     polys[i].onclick = () => openOverlay(polys[i].src);
   }
 });
