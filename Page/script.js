8f9c267d387183f60ab897fb0c3639694475cc84
Maurice Huchler <maurice.huchler@web.de>
2023-08-10 16:26:24 +0200

    added prismjs + removed redundant functions

--- Page/script.js
+++ Page/script.js
@@ -40,19 +40,4 @@ window.addEventListener("load", function () {
   }
 });
 
-// copy to clipboard
-function copyToClipboard(copyText) {
-  // Get the text field
-  // var copyText = document.getElementById(id);
-
-  // // Select the text field
-  // copyText.select();
-  // copyText.setSelectionRange(0, 99999); // For mobile devices
-
-  // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText);
-  
-  // Alert the copied text
-  alert("Copied the text: " + copyText);
-}
 
